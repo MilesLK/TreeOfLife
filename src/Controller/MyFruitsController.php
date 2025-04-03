ea22ee96078feba4aba1ad7f74211fdd622b7ddf
MilesLK <bricefrancietta@hotmail.com>
2025-04-03 12:07:37 +0200

    Mise en place du formulaire d'ajout, mise en place de la vue des ajouts dans la base de donnée

--- src/Controller/MyFruitsController.php
+++ src/Controller/MyFruitsController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\FruitsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MyFruitsController extends AbstractController{
     #[Route('/my/fruits', name: 'app_my_fruits')]
-    public function index(): Response
+    public function index(FruitsRepository $fruitsRepository): Response
     {
+        $elements = $fruitsRepository->findAll();
+
         return $this->render('my_fruits/index.html.twig', [
-            'controller_name' => 'MyFruitsController',
+            'elements' => $elements,
         ]);
     }
 }
